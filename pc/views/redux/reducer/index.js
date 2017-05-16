049b7f13d48aee9a9c20c177dfa7fb6a622085af
Cynric777 <383467294@qq.com>
2017-05-17 00:26:15 +0800

    add stage

--- pc/views/redux/reducer/index.js
+++ pc/views/redux/reducer/index.js
@@ -4,9 +4,11 @@ import { combineReducers } from 'redux';
 import dock from './dock';
 import logic from './logic';
 import observe from './observe';
+import stage from './stage';
 
 export default combineReducers({
     dock,
     logic,
-    observe
+    observe,
+    stage
 })
