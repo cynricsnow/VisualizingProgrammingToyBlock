adb849aa6126c59ae982e7930de73ec7d06bf5cd
Cynric777 <383467294@qq.com>
2017-05-19 16:19:14 +0800

    remove useless route

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -29,14 +29,4 @@ router.post('/update', (req, res) => {
     });
 })
 
-router.get('/input_distance', (req, res) => {
-    const distance = (Math.random() * 100).toFixed(2);
-    res.status(200).json(distance);
-});
-
-router.get('/input_temperature', (req, res) => {
-    const temperature = (Math.random() * 80 - 30).toFixed(1);
-    res.status(200).json(temperature);
-});
-
 module.exports = router;
