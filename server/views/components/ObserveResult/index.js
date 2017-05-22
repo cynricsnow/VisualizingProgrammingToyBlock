7f64b680745f87f735f8014469cfa0712871ea38
Cynric777 <383467294@qq.com>
2017-05-22 18:49:15 +0800

    change to mock from remote

--- server/views/components/ObserveResult/index.js
+++ server/views/components/ObserveResult/index.js
@@ -27,36 +27,40 @@ let width = 0;
 )
 class ObserveResult extends Component {
     input_temperature() {
-        const { src, dest } = this.props;
-        if (src) {
-            const temperature = +JSON.parse($.ajax({
-                type: 'POST',
-                url: `http://${src}:8000/input_temperature`,
-                async: false,
-                data: {
-                    dest
-                },
-                xhrFields: {'Access-Control-Allow-Origin': '*' }
-            }).responseText);
-            return temperature;
-        }
-        return;
+        const temperature = (Math.random() * 80 - 30).toFixed(1)
+        return temperature;
+        // const { src, dest } = this.props;
+        // if (src) {
+        //     const temperature = +JSON.parse($.ajax({
+        //         type: 'POST',
+        //         url: `http://${src}:8000/input_temperature`,
+        //         async: false,
+        //         data: {
+        //             dest
+        //         },
+        //         xhrFields: {'Access-Control-Allow-Origin': '*' }
+        //     }).responseText);
+        //     return temperature;
+        // }
+        // return;
     }
     input_distance() {
-        const { src, dest } = this.props;
-        if (src) {
-            const distance = +JSON.parse($.ajax({
-                type: 'POST',
-                url: `http://${src}:8000/input_distance`,
-                async: false,
-                data: {
-                    dest
-                },
-                xhrFields: {'Access-Control-Allow-Origin': '*' }
-            }).responseText);
-            return distance;
-        }
-        return;
+        const distance = (Math.random() * 100).toFixed(2);
+        return distance;
+        // const { src, dest } = this.props;
+        // if (src) {
+        //     const distance = +JSON.parse($.ajax({
+        //         type: 'POST',
+        //         url: `http://${src}:8000/input_distance`,
+        //         async: false,
+        //         data: {
+        //             dest
+        //         },
+        //         xhrFields: {'Access-Control-Allow-Origin': '*' }
+        //     }).responseText);
+        //     return distance;
+        // }
+        // return;
     }
     output_forward(distance) {
         const times = distance / 20;
